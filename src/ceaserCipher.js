72f46082b72f4efb337654a1c8b74ac6f0b197b2
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-07-02 10:50:48 +0530

    Wrote ceaserCipher function

---
+++ src/ceaserCipher.js
@@ -0,0 +1,73 @@
+const ceaserCipher = (string, shift) => {
+  const encryption = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+
+  const decryption = {
+    1: "a",
+    2: "b",
+    3: "c",
+    4: "d",
+    5: "e",
+    6: "f",
+    7: "g",
+    8: "h",
+    9: "i",
+    10: "j",
+    11: "k",
+    12: "l",
+    13: "m",
+    14: "n",
+    15: "o",
+    16: "p",
+    17: "q",
+    18: "r",
+    19: "s",
+    20: "t",
+    21: "u",
+    22: "v",
+    23: "w",
+    24: "x",
+    25: "y",
+    26: "z",
+  };
+
+  let ectryptedString = "";
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] in encryption) {
+      key = encryption[string[i]];
+      ectryptedString += decryption[(key + shift) % 26];
+    } else {
+      ectryptedString += string[i];
+    }
+  }
+
+  return ectryptedString;
+};
+
+module.exports = ceaserCipher;
